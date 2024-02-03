c2ecb99c9ad21c2ee62cd3d6883a03c6b4746249
Natalie <natalie.yeung16@gmail.com>
2024-02-03 15:33:19 -0700

    Add divide line component

--- calpal/src/app/page.tsx
+++ calpal/src/app/page.tsx
@@ -1,18 +1,20 @@
 import Image from "next/image";
 import Welcome from "./components/welcome";
 import Mission from "./components/mission";
-import Stats from "./components/stats"
+import Stats from "./components/stats";
 import How from "./components/how";
 import Banner from "./components/banner";
+import DivideLine from "./components/divideLine";
 
 export default function Landing() {
-    return (
-        <div className="">
-            <Welcome/>
-            <Mission />
-            <Stats />
-            <How />
-            <Banner />
-        </div>
-    );
+  return (
+    <div className=" bg-white">
+      <Welcome />
+      <Mission />
+      <DivideLine />
+      <Stats />
+      <How />
+      <Banner />
+    </div>
+  );
 }
