d43d2bac9c5b8b433cc5f746c4d03a2d5cdc7def
repl-haseeb-khan <81185655+repl-haseeb-khan@users.noreply.github.com>
2024-02-03 13:34:16 -0700

    change placeholders in register

--- calpal/src/app/components/registerForm.tsx
+++ calpal/src/app/components/registerForm.tsx
@@ -12,19 +12,19 @@ const RegisterPage = () => {
     const [confirmPassword, setConfirmPassword] = useState('');
 
     const handleSubmit = () => {
-        // Handle form submission
+        //to be implemented
     };
 
     return (
         <div>
             <div className="flex flex-col items-center justify-center w-full flex-1 flex-col bg-light-cream">
-                <Field labelName='First Name' fieldType='text' fieldValue={firstName} onChange={setFirstName} placeholderText='First Name' />
-                <Field labelName='Last Name' fieldType='text' fieldValue={lastName} onChange={setLastName} placeholderText='Last Name' />
-                <Field labelName='Address' fieldType='text' fieldValue={address} onChange={setAddress} placeholderText='Address' />
-                <Field labelName='Email' fieldType='email' fieldValue={email}  onChange={setEmail} placeholderText='Email Address' />
-                <Field labelName='Password' fieldType='password' fieldValue={password} onChange={setPassword} placeholderText='A secure password'  />
+                <Field labelName='First Name' fieldType='text' fieldValue={firstName} onChange={setFirstName} placeholderText='Enter your first name' />
+                <Field labelName='Last Name' fieldType='text' fieldValue={lastName} onChange={setLastName} placeholderText='Enter your last name' />
+                <Field labelName='Address' fieldType='text' fieldValue={address} onChange={setAddress} placeholderText='Enter your address' />
+                <Field labelName='Email' fieldType='email' fieldValue={email}  onChange={setEmail} placeholderText='Enter your email address' />
+                <Field labelName='Password' fieldType='password' fieldValue={password} onChange={setPassword} placeholderText='Enter a secure password'  />
                 <Field labelName='Confirm Password' fieldType='password' fieldValue={confirmPassword} onChange={setConfirmPassword} placeholderText='Re-enter your secure password'  />
-                <Button buttonText='Submit' onSubmit={handleSubmit} />
+                <Button buttonText='Submit' onClick={handleSubmit} />
             </div>
         </div>
     );
