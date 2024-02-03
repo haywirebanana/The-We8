75f51ba0e351dcc887f8d0384ae9863b993415b6
repl-haseeb-khan <81185655+repl-haseeb-khan@users.noreply.github.com>
2024-02-03 13:06:35 -0700

    use fields and buttons in register page

--- calpal/src/app/components/registerForm.tsx
+++ calpal/src/app/components/registerForm.tsx
@@ -1,8 +1,9 @@
 "use client"
 import { useState } from 'react';
+import Field from './field';
+import Button from './button';
 
 const RegisterPage = () => {
-    const [step, setStep] = useState(1);
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [address, setAddress] = useState('');
@@ -10,95 +11,21 @@ const RegisterPage = () => {
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
 
-    const handleNextStep = () => {
-        setStep(step + 1);
-    };
-
-    const handlePreviousStep = () => {
-        setStep(step - 1);
-    };
-
     const handleSubmit = () => {
         // Handle form submission
     };
 
     return (
         <div>
-            {step === 1 && (
-                <div className="flex flex-col items-center justify-center w-full flex-1 px-40 flex-col bg-light-cream">
-                    <label>
-                        First Name:
-                        <br />
-                        <input
-                            type="text"
-                            value={firstName}
-                            onChange={(e) => setFirstName(e.target.value)}
-                        />
-                    </label>
-                    <br />
-                    <label>
-                        Last Name:
-                        <br />
-                        <input
-                            type="text"
-                            value={lastName}
-                            onChange={(e) => setLastName(e.target.value)}
-                        />
-                    </label>
-                    <br />
-                    <label>
-                        Address:
-                        <br />
-                        <input
-                            type="text"
-                            value={address}
-                            onChange={(e) => setAddress(e.target.value)}
-                        />
-                    </label>
-                    <br />
-                    <label>
-                        Email:
-                        <br />
-                        <input
-                            type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </label>
-                    <br />
-                    <label>
-                        Password:
-                        <br />
-                        <input
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </label>
-                    <br />
-                    <label>
-                        Confirm Password:
-                        <br />
-                        <input
-                            type="password"
-                            value={confirmPassword}
-                            onChange={(e) => setConfirmPassword(e.target.value)}
-                        />
-                    </label>
-                    <br />
-                    <button onClick={handleNextStep}>Next</button>
-                </div>
-            )}
-
-            {/* Add more steps here */}
-
-            {step > 1 && (
-                <button onClick={handlePreviousStep}>Previous</button>
-            )}
-
-            {step === 3 && (
-                <button onClick={handleSubmit}>Submit</button>
-            )}
+            <div className="flex flex-col items-center justify-center w-full flex-1 flex-col bg-light-cream">
+                <Field labelName='First Name' fieldType='text' fieldValue={firstName} onChange={setFirstName} placeholderText='First Name' />
+                <Field labelName='Last Name' fieldType='text' fieldValue={lastName} onChange={setLastName} placeholderText='Last Name' />
+                <Field labelName='Address' fieldType='text' fieldValue={address} onChange={setAddress} placeholderText='Address' />
+                <Field labelName='Email' fieldType='email' fieldValue={email}  onChange={setEmail} placeholderText='Email Address' />
+                <Field labelName='Password' fieldType='password' fieldValue={password} onChange={setPassword} placeholderText='A secure password'  />
+                <Field labelName='Confirm Password' fieldType='password' fieldValue={confirmPassword} onChange={setConfirmPassword} placeholderText='Re-enter your secure password'  />
+                <Button buttonText='Submit' onSubmit={handleSubmit} />
+            </div>
         </div>
     );
 };
