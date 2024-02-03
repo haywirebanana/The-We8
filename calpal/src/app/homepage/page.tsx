1bbaefa1df06cc5c130dfccfe877dcc31fc9b2d8
haywirebanana <113632239+haywirebanana@users.noreply.github.com>
2024-02-03 14:30:26 -0700

    getting updates

---
+++ calpal/src/app/homepage/page.tsx
@@ -0,0 +1,10 @@
+export default function Penis() {
+
+    return (
+    <div className="flex flex-col items-center justify-center w-full flex-1 min-h-screen flex-col bg-green">
+        <div className="bg-light-cream rounded-xl p-12 w-2/3">
+                <h1>Let&apos;s get you logged in!</h1>
+        </div>
+    </div>
+    );
+};
