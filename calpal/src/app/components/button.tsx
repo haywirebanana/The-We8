24d3a5fe2df0481073249361068b1fcd43c121dd
repl-haseeb-khan <81185655+repl-haseeb-khan@users.noreply.github.com>
2024-02-03 13:33:47 -0700

    update button

--- calpal/src/app/components/button.tsx
+++ calpal/src/app/components/button.tsx
@@ -2,17 +2,13 @@
 
 interface ButtonProps {
   buttonText?: string;
-  onSubmit: (value: string) => void;
+  onClick: () => void;
 }
 
-export default function Button({ buttonText, onSubmit }: ButtonProps) {
-  const handleSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
-    onSubmit(e.target.value);
-  }
-
+export default function Button({ buttonText, onClick }: ButtonProps) {
   return (
     <div className="my-2">
-      <button type="button" className="text-white bg-clay hover:bg-sand focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{buttonText}</button>
+      <button type="button" onClick={onClick} className="text-white bg-clay hover:bg-sand focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{buttonText}</button>
     </div>
   )
 }
