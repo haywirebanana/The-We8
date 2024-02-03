99b8da9de52b8cb72e5d4c2ed33f095a105cd3ae
Natalie <natalie.yeung16@gmail.com>
2024-02-03 16:39:47 -0700

    Add how-to squares and numbers

--- calpal/src/app/components/how.tsx
+++ calpal/src/app/components/how.tsx
@@ -1,25 +1,97 @@
-'use client'
+"use client";
 import Image from "next/image";
 
 export default function How() {
-    return (
-        <div className="flex h-3753 flex-col items-center justify-between bg-white">
-                <div className="w-full justify-between py-5">
-                        <header className="text-black font-serif text-4xl font-normal">
-                            How it works
-                        </header>
-                </div>
-                <div className="flex flex-row justify-between w-1/2">
-                    <div className="w-287 h-287 rounded-lg bg-clay">
-                        Join Program
-                    </div>
-                    <div className="w-287 h-287 rounded-lg bg-clay">
-                        Get Matched
-                    </div>
-                    <div className="w-287 h-287 rounded-lg bg-clay">
-                        Build Outcome
-                    </div>
-                </div>
+  return (
+    <div className="flex h-3753 flex-col items-center justify-between bg-white">
+      <div className="w-full justify-between py-5">
+        <header className="text-black  font-serif text-5xl py-4 text-center mx-10 my-10">
+          How it works
+        </header>
+      </div>
+      <div className="mx-10 mb-10" style={{ display: "flex" }}>
+        <div style={{ position: "relative", display: "inline" }}>
+          <div
+            className="rounded-3xl bg-clay mx-10"
+            style={{
+              width: "300px",
+              height: "300px",
+              backgroundColor: "#BC6C25",
+            }}
+          ></div>
+          <div
+            style={{
+              position: "absolute",
+              top: "20px",
+              left: "60px",
+              width: "50px",
+              height: "50px",
+              borderRadius: "50%",
+              backgroundColor: "#FAEDCD", // Customize the circle's color
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "#BC6C25", // Customize the text color
+            }}
+          >
+            1
+          </div>
         </div>
-    );
+        <div style={{ position: "relative", display: "inline-block" }}>
+          <div
+            className="rounded-3xl bg-clay mx-10"
+            style={{
+              width: "300px",
+              height: "300px",
+              backgroundColor: "#BC6C25",
+            }}
+          ></div>
+          <div
+            style={{
+              position: "absolute",
+              top: "20px",
+              left: "60px",
+              width: "50px",
+              height: "50px",
+              borderRadius: "50%",
+              backgroundColor: "#FAEDCD", // Customize the circle's color
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "#BC6C25", // Customize the text color
+            }}
+          >
+            2
+          </div>
+        </div>
+        <div style={{ position: "relative", display: "inline-block" }}>
+          <div
+            className="rounded-3xl bg-clay mx-10"
+            style={{
+              width: "300px",
+              height: "300px",
+              backgroundColor: "#BC6C25",
+            }}
+          ></div>
+          <div
+            style={{
+              position: "absolute",
+              top: "20px",
+              left: "60px",
+              width: "50px",
+              height: "50px",
+              borderRadius: "50%",
+              backgroundColor: "#FAEDCD", // Customize the circle's color
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              color: "#BC6C25", // Customize the text color
+            }}
+          >
+            3
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
