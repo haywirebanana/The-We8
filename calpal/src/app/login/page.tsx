2ca606872458e731743c97d9739fed591c25b82d
Haseeb Adil Khan <haseebadilkhan@gmail.com>
2024-02-03 01:55:52 -0700

    create login page, clear up landing page

---
+++ calpal/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Login() {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+                <div className="bg-purple">
+                        <h1>CalPal will do amazing things.</h1>
+                </div>
+        </div>
+    );
+}
