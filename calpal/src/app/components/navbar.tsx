e16a03e0eb650b6dd2600455e70ffd4a1643d445
arminsandhu <113546432+arminsandhu@users.noreply.github.com>
2024-02-03 16:49:35 -0700

    icons on nav

--- calpal/src/app/components/navbar.tsx
+++ calpal/src/app/components/navbar.tsx
@@ -1,11 +1,12 @@
 'use client';
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname} from 'next/navigation';
 
 const Navbar = () => {
     const pathname = usePathname()
 
-    const auth = 0;
+    const auth = 1;
 
     const getButtonText = () => {
         if(auth) {
@@ -24,25 +25,27 @@ const Navbar = () => {
     };
     
   return (
-        <div className="flex-none flex flex-row justify-between bg-white py-3">
-            <div className="flex items-center w-1/3 justify-between px-10">
-                <div className="text-clay">
-                    LOGO
-                </div>
-                <div>
-                    <Link className="text-clay" href="/">Haseeb</Link>   
-                </div>
-                <div>
-                    <Link className="text-clay" href="/ryan">Ryan</Link>
-                </div>
-                <div>
-                    <Link className="text-clay" href="/armin">Armin</Link>
-                </div>
-                <div>
-                    <Link className="text-clay" href="/armin">Link</Link>
-                </div>
+        <div className="flex-none flex flex-row justify-between bg-white py-3 pl-20">
+            <div className="flex items-center w-5/6">
+                <Image src="/logo.png" width={75} height={50} alt="lgog"/>
+                {auth ? (
+                <div className="flex flex-row justify-between w-1/4">
+                    <div>
+                        <Link className="text-clay" href="/">Haseeb</Link>   
+                    </div>
+                    <div>
+                        <Link className="text-clay" href="/ryan">Ryan</Link>
+                    </div>
+                    <div>
+                        <Link className="text-clay" href="/armin">Armin</Link>
+                    </div>
+                    <div>
+                        <Link className="text-clay" href="/armin">Link</Link>
+                    </div>
+                </div>    
+                ) : ('')}
             </div>
-            <div className="flex">
+            <div className="flex w-1/6">
                 {/* {auth ? (
                     <button className=" hover:bg-sand focus:outline-none focus:ring-4 focus:ring-blue-300 px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 text-white font-roboto h-14 w-48 rounded-3xl font-light text-3xl bg-clay hover:dark-brown">
                         {getButtonText()}
@@ -50,9 +53,9 @@ const Navbar = () => {
                 ) : (
                     <h4>hi</h4>
                 )} */}
-                <button className=" hover:bg-sand focus:outline-none focus:ring-4 focus:ring-blue-300 px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 text-white font-roboto h-14 w-48 rounded-3xl font-light text-3xl bg-clay hover:dark-brown">
+                <Link href="/login" className=" hover:bg-sand focus:outline-none focus:ring-4 focus:ring-blue-300 px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 text-white font-roboto h-14 w-48 rounded-3xl font-light text-2xl bg-clay hover:dark-brown">
                     {getButtonText()}
-                </button>
+                </Link>
             </div>
     </div>
   );
