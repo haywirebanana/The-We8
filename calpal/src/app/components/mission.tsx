e0f58a3f0b7a1311015c3077ebe96802bb0f265c
Natalie <natalie.yeung16@gmail.com>
2024-02-03 14:00:43 -0700

    Add css to mission section on landing

--- calpal/src/app/components/mission.tsx
+++ calpal/src/app/components/mission.tsx
@@ -1,29 +1,32 @@
-'use client'
+"use client";
 import Image from "next/image";
 
 export default function Mission() {
-    return (
-        <div className="flex h-716 flex-row items-center justify-between bg-white">
-                <div className="w-1/2">
-                    we will add something here
-                </div>
-                <div className="flex flex-col h-full w-1/2 justify-between py-5">
-                        <header className="text-black font-serif text-4xl font-normal">
-                            Mission
-                        </header>
-                        <h2 className="text-black font-montserrat font-normal leading-5">
-                            Lorem ipsum dolor sit amet, 
-                            consectetur adipiscing elit, sed do 
-                            eiusmod tempor incididunt ut labore et 
-                            dolore magna aliqua.
-                        </h2>
-                        <h2 className="text-black font-montserrat font-normal leading-5">
-                            Lorem ipsum dolor sit amet, 
-                            consectetur adipiscing elit, sed do 
-                            eiusmod tempor incididunt ut labore et 
-                            dolore magna aliqua.
-                        </h2>
-                </div>
-        </div>
-    );
+  return (
+    <div className="flex h-716 flex-row items-center justify-between bg-white">
+      <div
+        className="box-content h-80 mx-auto my-20 border-solid border-orange-500 rounded-3xl	"
+        style={{ width: "500px", height: "500px", backgroundColor: "#DDA15E" }}
+      >
+        we will add something here
+      </div>
+      <div className="flex flex-col h-full w-1/2 justify-between">
+        <header className="text-black font-serif text-4xl font-normal mt-[-20px]">
+          Mission
+        </header>
+        <h2 className="text-black font-montserrat font-normal leading-5 pe-40 pt-10">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua.
+        </h2>
+        <h2 className="text-black font-montserrat font-normal leading-5 pe-40 pt-10">
+          Lorem ipsum dolor sit amet, consecstetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua.
+        </h2>
+      </div>
+    </div>
+  );
 }
