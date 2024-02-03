f1eb2954dddfdab00009731f2cc26a58ee361d74
arminsandhu <113546432+arminsandhu@users.noreply.github.com>
2024-02-03 10:40:07 -0700

    fiixng the root dirextory

--- calpal/src/app/page.tsx
+++ calpal/src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import Welcome from "./components/welcome";
+import Mission from "./components/mission";
+import Stats from "./components/stats"
+import How from "./components/how";
+import Banner from "./components/banner";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>CalPal will do amazing things.</h1>
-    </main>
-  );
+export default function Landing() {
+    return (
+        <div className="h-3753">
+            <Welcome />
+            <Mission />
+            <Stats />
+            <How />
+            <Banner />
+        </div>
+    );
 }
