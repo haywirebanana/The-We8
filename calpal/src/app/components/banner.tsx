c9b9d0b93800fcf022d0b6a6bba8e539ae5824eb
arminsandhu <113546432+arminsandhu@users.noreply.github.com>
2024-02-03 14:22:29 -0700

    banner + stats formatting

--- calpal/src/app/components/banner.tsx
+++ calpal/src/app/components/banner.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export default function Banner() {
     return (
-        <div className="flex h-420 flex-row items-center justify-between bg-green">
-                <div className="flex flex-col h-full w-1/2 justify-between py-5">
+        <div className="flex h-96 items-center justify-center bg-green">
+                <div className="flex flex-col h-1/6 w-1/2 items-center justify-between py-5">
                         <header className="text-white font-serif text-4xl font-normal">
                             Name of Program
                         </header>
