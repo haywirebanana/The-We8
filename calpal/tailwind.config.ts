a518e46ce7a449daba3e62e24f337360da5cb530
Haseeb Adil Khan <haseebadilkhan@gmail.com>
2024-02-03 01:52:47 -0700

    setup tailwind colours

--- calpal/tailwind.config.ts
+++ calpal/tailwind.config.ts
@@ -7,12 +7,18 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+//      transparent: 'transparent',
+//      current: 'currentColor',
+      'dark-green': '#283618',
+      'forest-green': '#606C38',
+      'green': '#CCD5AE',
+      'light-green': '#E9EDC9',
+      'clay': '#BC6C25',
+      'sand': '#DDA15E',
+      'cream': '#FAEDCD',
+      'light-cream': '#FEFAE0',
+      'white': '#FFFFFF',
     },
   },
   plugins: [],
