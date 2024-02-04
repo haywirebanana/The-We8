e0398df75d0c145b7117a7b550017e8c9fac4095
arminsandhu <113546432+arminsandhu@users.noreply.github.com>
2024-02-03 22:12:51 -0700

    um hello

--- calpal/src/app/home/page.tsx
+++ calpal/src/app/home/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
                 <div className="flex h-1/3 items-center justify-center bg-green">
                     <header className="text-white font-serif text-4xl font-normal">Name of Program</header>
                 </div>
-                <div className="flex h-2/3 bg-white justify-center">
+                <div className="flex h-2/3  bg-white justify-center">
                     <header className="text-black font-serif text-4xl font-normal">
                         Mentors
                     </header>
