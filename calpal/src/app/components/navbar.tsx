40b474fe5e9fbbf1e2b62be8e1a72f12bbbea869
repl-haseeb-khan <81185655+repl-haseeb-khan@users.noreply.github.com>
2024-02-04 09:06:15 -0700

    styling and routing changes

--- calpal/src/app/components/navbar.tsx
+++ calpal/src/app/components/navbar.tsx
@@ -2,41 +2,48 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from 'react';
-import { usePathname} from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
     const pathname = usePathname()
 
     const getButtonText = () => {
-      switch (pathname) {
-        case '/register':
-          return 'Login';
-        case '/login':
-          return 'Register';
-        case '/about':
-          return 'Contact Us';
-        default:
-          return 'Login';
-      }
+        switch (pathname) {
+            case '/register':
+                return 'Login';
+            case '/login':
+                return 'Register';
+            case '/about':
+                return 'Contact Us';
+            default:
+                return 'Login';
+        }
     };
-  return (
+
+    const getPath = () => {
+        switch (pathname) {
+            case '/register':
+                return '/login';
+            case '/login':
+                return '/register';
+            case '/about':
+                return 'Contact Us';
+            default:
+                return 'Login';
+        }
+    };
+
+    return (
         <div className="flex-none flex flex-row justify-between bg-white py-3 pl-20">
             <div className="flex items-center w-5/6">
-                <Image src="/logo.png" width={75} height={50} alt="lgog"/>
+                <Image src="/logo.png" width={75} height={50} alt="lgog" />
             </div>
             <div className="flex w-1/6">
-                {/* {auth ? (
-                    <button className=" hover:bg-sand focus:outline-none focus:ring-4 focus:ring-blue-300 px-5 py-2.5 text-center mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 text-white font-roboto h-14 w-48 rounded-3xl font-light text-3xl bg-clay hover:dark-brown">
-                        {getButtonText()}
-                    </button>
-                ) : (
-                    <h4>hi</h4>
-                )} */}
-                <Link href="/login" className=" hover:bg-sand focus:outline-none focus:ring-4 focus:ring-blue-300 px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 text-white font-roboto h-14 w-48 rounded-3xl font-light text-2xl bg-clay hover:dark-brown">
+                <Link href={getPath()} className="hover:bg-sand focus:outline-none focus:ring-4 focus:ring-blue-300 px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 text-white font-roboto h-14 w-48 rounded-3xl font-light text-2xl bg-clay hover:dark-brown">
                     {getButtonText()}
                 </Link>
             </div>
-    </div>
-  );
+        </div>
+    );
 };
 export default Navbar;
