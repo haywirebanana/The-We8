0cfb760969e195745502974d8fb9684dc6bac9db
Natalie <natalie.yeung16@gmail.com>
2024-02-04 00:05:44 -0700

    Add landing content

--- calpal/src/app/components/stats.tsx
+++ calpal/src/app/components/stats.tsx
@@ -1,35 +1,47 @@
-'use client'
+"use client";
 import Image from "next/image";
 
 export default function Stats() {
-    return (
-        <div className="flex h-dvh flex-row items-center  bg-white">
-                <div className="flex flex-col h-full w-1/2 px-24">
-                    <div>
-                        <header className="text-black w-112 font-serif text-5xl py-4">
-                            Statistics and current barriers
-                        </header>
-                    </div>
-                    <div>
-                        <h2 className="text-black font-montserrat text-2xl font-normal leading-8">
-                            Lorem ipsum dolor sit amet, 
-                            consectetur adipiscing elit, sed do 
-                            eiusmod tempor incididunt ut labore et 
-                            dolore magna aliqua.
-                        </h2>
-                    </div>
-                </div>
-                <div className="flex flex-col items-center justify-center w-1/2 py-5 align-middle">
-                    <div className="w-2/3 h-48 rounded-3xl bg-light-green my-5">
-                        hello
-                    </div>
-                    <div className="w-2/3 h-48 rounded-3xl bg-green my-5">
-                        hello
-                    </div>
-                    <div className="w-2/3 h-48 rounded-3xl bg-dark-green my-5">
-                        hello
-                    </div>
-                </div>
+  return (
+    <div className="flex h-dvh flex-row items-center  bg-white">
+      <div className="flex flex-col h-full w-1/2 px-24">
+        <div>
+          <header className="text-black w-112 font-serif text-5xl py-4">
+            The Numbers and
+            <br />
+            Existing Challenges
+          </header>
         </div>
-    );
+        <div>
+          <h2
+            className="text-black font-montserrat font-normal pe-40 pt-10"
+            style={{ fontSize: "18px" }}
+          >
+            Calgary offers an extensive array of services to support newcomers,
+            addressing needs ranging from language skills to various other
+            aspects.
+            <br />
+            <br />
+            However, a significant number of individuals are unaware or face
+            barriers to accessing these available resources.
+          </h2>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center w-1/2 py-5 align-middle">
+        <div className="w-2/3 h-48 rounded-3xl bg-light-green text-283618 text-2xl px-5 my-5 flex items-center justify-center text-center">
+          According to a 2021 survey, only 8% of respondents learned about
+          available services at a government office upon landing.
+        </div>
+        <div className="w-2/3 h-48 rounded-3xl bg-green text-283618 text-2xl px-5 my-5 flex items-center justify-center text-center">
+          There is no official process or location (physical or web-based) to
+          communicate information on settlement services.
+        </div>
+        <div className="w-2/3 h-60 rounded-3xl bg-dark-green text-white text-2xl px-5 my-5 flex items-center justify-center text-center">
+          In Alberta, there are more than 90 programs and resources, requiring
+          newcomers to navigate a complex landscape independently, dealing with
+          intricacies, and logistical considerations.
+        </div>
+      </div>
+    </div>
+  );
 }
--- calpal/src/app/components/welcome.tsx
+++ calpal/src/app/components/welcome.tsx
@@ -1,45 +1,49 @@
-'use client'
-import Button from './button';
-import Link from 'next/link';
+"use client";
+import Button from "./button";
+import Link from "next/link";
 
 export default function Welcome() {
+  const handleSubmit = () => {
+    //to be implemented
+  };
 
-    const handleSubmit = () => {
-        //to be implemented
-    };
-
-
-    return (
-        <div className="flex h-dvh flex-row items-center bg-forest-green">
-                <div className="flex flex-col h-5/6 w-1/2 align-center justify-center pl-24 mb-28">
-                    <div>
-                        <header className="text-white w-96 font-serif text-7xl font-normal py-4 leading-14">
-                            Welcome to CalPal
-                        </header>
-                    </div>
-                    <div>
-                        <h2 className="text-white font-sans font-light w-112 text-2xl leading-8 pb-10">
-                            Lorem ipsum dolor sit amet, 
-                            consectetur adipiscing elit, sed do 
-                            eiusmod tempor incididunt ut labore et 
-                            dolore magna aliqua.
-                        </h2>
-                    </div>
-                    <div>
-                        <Link href="/register" className=" hover:bg-sand focus:outline-none focus:ring-4 focus:ring-blue-300 px-20 py-3 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 text-white font-roboto rounded-3xl font-light text-3xl bg-clay hover:dark-brown">
-                            Join 
-                        </Link>
-
-                    </div>
-                    <div>
-                        <h3 className="text-white text-lg pt-4">
-                            Have an account? <a href="/login" className="text-light-green hover:underline py-5">Sign in</a>
-                        </h3>
-                    </div>
-                </div>
-                {/* <div className="w-1/2">
+  return (
+    <div className="flex h-dvh flex-row items-center bg-forest-green">
+      <div className="flex flex-col h-5/6 w-1/2 align-center justify-center pl-24 mb-28">
+        <div>
+          <header className="text-white w-96 font-serif text-5xl font-normal py-4 leading-14">
+            Bridging Lives, Building Futures{" "}
+          </header>
+        </div>
+        <div>
+          <h2 className="text-white font-sans font-light w-112 text-2xl leading-8 pb-10">
+            Introducing CalPal, a community-driven initiative dedicated to
+            empowering newcomers in Calgary.
+          </h2>
+        </div>
+        <div>
+          <Link
+            href="/register"
+            className=" hover:bg-sand focus:outline-none focus:ring-4 focus:ring-blue-300 px-20 py-3 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 text-white font-roboto rounded-3xl font-light text-3xl bg-clay hover:dark-brown"
+          >
+            Join
+          </Link>
+        </div>
+        <div>
+          <h3 className="text-white text-lg pt-4">
+            Have an account?{" "}
+            <a
+              href="/login"
+              className="text-light-green hover:underline py-5 mt-3"
+            >
+              Sign in
+            </a>
+          </h3>
+        </div>
+      </div>
+      {/* <div className="w-1/2">
                     we will add something here
                 </div> */}
-        </div>
-    );
+    </div>
+  );
 }
