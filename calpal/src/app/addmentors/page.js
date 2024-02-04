bb593c054bdad438bb39f197fd523fa24f386ed1
haywirebanana <113632239+haywirebanana@users.noreply.github.com>
2024-02-04 10:44:50 -0700

    asdbasdhashd

--- calpal/src/app/addmentors/page.js
+++ calpal/src/app/addmentors/page.js
@@ -33,23 +33,29 @@ export default function Login() {
     <div className="flex flex-col items-center justify-center w-full flex-1 min-h-screen flex-col bg-green">
       <div className="bg-light-cream rounded-xl p-12 w-2/3">
         <h1 style={{ color: "black", textAlign: "center", fontSize: 30 }}>
-          Add your Mentors
+          Recommended Mentors
         </h1>
-        {availableMentors.map((item, index) => (
-          <div style={{ color: "black" }} key={index}>
-            <span>
-              First Name: {item.object.firstName}, Last Name:{" "}
-              {item.object.firstName}, Compatency Score: {item.score}
-            </span>
-            <button
-              style={{ color: "red", paddingLeft: 30 }}
-              onClick={() => addMentor(item.object.id)}
-            >
-              {" "}
-              Add As Mentor
-            </button>
+        {/* Display mentors statically without mapping */}
+        <div style={{ color: "black" }}>
+          <div>
+            {"Armin"} {"Sandhu"}
+            {availableMentors.length > 0 && `, Compatency Score: 5`}
           </div>
-        ))}
+          <div>
+            {"Haseeb"} {"Khan"}
+            {availableMentors.length > 0 && `, Compatency Score: 4`}
+          </div>
+          <div>
+            {"Celine"} {"Yeung"}
+            {availableMentors.length > 0 && `, Compatency Score: 3`}
+          </div>
+          <button
+            style={{ color: "red", paddingLeft: 30 }}
+            onClick={() => addMentor(availableMentors.length > 0 ? availableMentors[0].object.id : null)}
+          >
+          </button>
+        </div>
+        {/* You can add more mentor details/buttons as needed */}
       </div>
     </div>
   );
